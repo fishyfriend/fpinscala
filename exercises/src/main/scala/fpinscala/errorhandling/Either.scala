25490c1b1615b62595f967e44f857d5a048412c2
fishyfriend <fishyfriend@users.noreply.github.com>
2017-09-10 01:45:20 -0400

    ex. 4.6 clean up Either methods

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -8,15 +8,15 @@ sealed trait Either[+E,+A] {
   // Ex. 4.6
   def map[B](f: A => B): Either[E, B] = this match {
     case Right(a) => Right(f(a))
-    case l @ Left(e) => l
+    case Left(e) => Left(e)
   }
   def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
     case Right(a) => f(a)
-    case l @ Left(e) => l
+    case Left(e) => Left(e)
   }
   def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-    case r @ Right(a) => r
-    case _ => b
+    case Right(a) => Right(a)
+    case Left(e) => b
   }
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
     for (aa <- this; bb <- b) yield f(aa, bb)
