a49b5b3e537a93128cd9397332663a9d16ab41b3
fishyfriend <fishyfriend@users.noreply.github.com>
2017-09-10 02:09:15 -0400

    ex. 4.7 clean up

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -22,10 +22,12 @@ sealed trait Either[+E,+A] {
     for (aa <- this; bb <- b) yield f(aa, bb)
 
   // Ex. 4.7
-  def sequence[E,A](es: List[Either[E,A]]): Either[E, List[A]] =
-    (es foldRight (Right(List[A]()): Either[E,List[A]])) { (e,l) => for(a<-e; as<-l) yield  a::as }
-  def traverse[E,A,B](as: List[A])(f: A=>Either[E,B]): Either[E, List[B]] =
-    (as foldRight (Right(List[B]()): Either[E,List[B]])) { (a, mlb) => for(bs<-mlb; b<-f(a)) yield (b::bs) }
+  def sequence[E, A](as: List[Either[E, A]]): Either[E, List[A]] = traverse(as)(a => a)
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    case Nil => Right(Nil)
+    case h :: t => (f(h) map2 traverse(t)(f(_))) { (a, as) => a :: as }
+  }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
